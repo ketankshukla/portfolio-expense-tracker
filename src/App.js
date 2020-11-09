9b32770e8fbf9285ff48a1bcd4b16b3534a6fbef
Ketan Shukla <ketankshukla@gmail.com>
2020-11-09 01:39:05 -0800

    cleanups

--- src/App.js
+++ src/App.js
@@ -1,31 +1,24 @@
-import React from 'react';
-import Header from './components/Header'
-import Balance from './components/Balance'
-import IncomeExpenses from './components/IncomeExpenses';
-import TransactionList from './components/TransactionList'
-import AddTransaction from './components/AddTransaction'
+import React from "react";
+import Header from "./components/Header";
+import Balance from "./components/Balance";
+import IncomeExpenses from "./components/IncomeExpenses";
+import TransactionList from "./components/TransactionList";
+import AddTransaction from "./components/AddTransaction";
 
-import './App.css'
-
+import "./App.css";
 
 const App = () => {
   return (
     <div>
-      <Header/>
+      <Header />
       <div className="container">
-        <Balance/>
-      </div>
-      <div className="container">
-        <IncomeExpenses/>
-      </div>
-      <div className="container">
-        <TransactionList/>
-      </div>
-      <div className="container">
-        <AddTransaction/>
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
       </div>
     </div>
   );
-}
+};
 
 export default App;
