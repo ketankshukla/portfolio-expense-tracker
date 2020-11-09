05c68556d3a22c223edfe200d003e2d28b64d291
Ketan Shukla <ketankshukla@gmail.com>
2020-11-09 01:24:33 -0800

    created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Header from './components/Header'
 import Balance from './components/Balance'
+import IncomeExpenses from './components/IncomeExpenses';
+import TransactionList from './components/TransactionList'
 
 import './App.css'
-import IncomeExpenses from './components/IncomeExpenses';
+
 
 const App = () => {
   return (
@@ -14,6 +16,9 @@ const App = () => {
       </div>
       <div className="container">
         <IncomeExpenses/>
+      </div>
+      <div className="container">
+        <TransactionList/>
       </div>
     </div>
   );
