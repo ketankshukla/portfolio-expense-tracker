1067890da20668b81b2554e25bd040b2ad207617
Ketan Shukla <ketankshukla@gmail.com>
2020-11-09 01:28:35 -0800

    created

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header'
 import Balance from './components/Balance'
 import IncomeExpenses from './components/IncomeExpenses';
 import TransactionList from './components/TransactionList'
+import AddTransaction from './components/AddTransaction'
 
 import './App.css'
 
@@ -19,6 +20,9 @@ const App = () => {
       </div>
       <div className="container">
         <TransactionList/>
+      </div>
+      <div className="container">
+        <AddTransaction/>
       </div>
     </div>
   );
