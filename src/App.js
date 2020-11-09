94e45d4561e3a0b99e35d167b7a1b408d0530d5d
Ketan Shukla <ketankshukla@gmail.com>
2020-11-09 01:01:19 -0800

    initialized

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from './components/Header'
 
 const App = () => {
   return (
     <div className="App">
-      Vanilla React App!
+      <Header/>
     </div>
   );
 }
