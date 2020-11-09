9af8c8781984d86221305add9bf2fb7f9f308c7c
Ketan Shukla <ketankshukla@gmail.com>
2020-11-09 02:37:31 -0800

    added

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -26,11 +26,19 @@ export const GlobalProvider = ({ children }) => {
       payload: id,
     });
   }
+
+  function addTransaction(transaction) {
+    dispatch({
+      type: "ADD_TRANSACTION",
+      payload: transaction,
+    });
+  }
+
   return (
     <GlobalContext.Provider
       value={{
         transactions: state.transactions,
-        deleteTransaction
+        deleteTransaction, addTransaction
       }}
     >
       {children}
