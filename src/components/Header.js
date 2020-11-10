8f1418e39fd739a1b5039077050c603b4bc7dfca
Ketan Shukla <ketankshukla@gmail.com>
2020-11-09 19:53:18 -0800

    header modified

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,12 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <h2>
-            Expense Tracker
-            By Ketan Shukla
-        </h2>
+        <div>
+            <h2>
+                <p>Expense Tracker</p>
+                <p>By Ketan Shukla</p>
+            </h2>
+        </div>
     )
 }
 
